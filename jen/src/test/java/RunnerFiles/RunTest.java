04f01ed49d15cea2f7d93da843babdeaffcd7d1d
sowmya <sowmya@DESKTOP-DKK5M0P>
2021-06-15 00:28:11 +0530

    1st Commit

---
+++ jen/src/test/java/RunnerFiles/RunTest.java
@@ -0,0 +1,11 @@
+package RunnerFiles;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+	@RunWith(Cucumber.class)
+	@CucumberOptions(
+	features = "Features",
+	glue = "StepDefinitions")
+	public class RunTest {
+}
